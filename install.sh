f0d35b9b4234e40e565f43f1dac365be62e43ecc
snlml <nkvvvd@gmail.com>
2025-05-07 17:39:25 +0400

    dotfiles

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+echo "Package install"
+
+sudo xbps-install -Syy
+sudo xbps-install -u xbps
+sudo xbps-install -Syu
+sudo xbps-install -Rs void-repo-nonfree 
+sudo xbps-install i3-gaps kitty rofi polybar feh zsh git wget curl neofetch 7zip unzip unrar nano ImageMagick htop flatpak qbittorrent dunst
+
+echo "Zsh/theme install"
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+set default zsh
+cp .zshrc ~/.zshrc
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+echo "Zsh/theme installed"
+
+echo "Dotfiles install"
+
+mv "$(dirname "$0")/config/"* "$(dirname "$0")/config"/.* ~/
+
+echo "Dotfiles installed!"
+
+echo "Font install"
+
+fc-cache -fv
